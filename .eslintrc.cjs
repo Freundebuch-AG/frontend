7924676b2ae3e362083ce65332facd936a71eeb7
ep-aotto <aotto@epages.com>
2023-04-17 20:20:47 +0200

    Page Layout

---
+++ .eslintrc.cjs
@@ -0,0 +1,66 @@
+const config = {
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: [
+    "plugin:import/recommended",
+    "plugin:promise/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:tailwindcss/recommended",
+    "standard-with-typescript",
+    "prettier", // Has to be set last!
+  ],
+  /** @type any[] */ overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+  plugins: [
+    "import",
+    "promise",
+    "jsx-a11y",
+    "tailwindcss",
+    "simple-import-sort",
+    "prettier",
+  ],
+  rules: {
+    "tailwindcss/no-custom-classname": "off", // Has to be disabled because the usage of tailwindcss plugins triggers this rule
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "@typescript-eslint/triple-slash-reference": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
+
+config.overrides.push({
+  files: ["*.astro"],
+  extends: [
+    ...config.extends,
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+  ],
+  parser: "astro-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    extraFileExtensions: [".astro"],
+  },
+});
+
+module.exports = config;
